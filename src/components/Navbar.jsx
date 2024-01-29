c3f553675c91636dbf3d3440c3e7d710fd95e573
Glitch (glitch-hello-react) <none>
2024-01-29 06:13:24 +0000

    🕰🏍 Checkpoint ./src/components/Navbar.jsx:36986232/1498 ./src/styles/Navbar.css:36986232/2395 ./src/styles/styles.css:36986232/4000

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
 const Navbar = () => {
-    // ... other states and functions
-
     return (
         <nav className="navbar">
-            <div className="navbar-logo">
-                <img src="https://cdn.glitch.global/4f194de8-562e-4c7b-bc7c-babcb839caf7/Vector.png?v=1706507904041" alt="Logo" />
+            <div className="navbar-left">
+                <img src="https://cdn.glitch.global/4f194de8-562e-4c7b-bc7c-babcb839caf7/Vector.png?v=1706507904041" alt="Logo" className="navbar-logo" />
+                <div className="navbar-title">Taylor Swift</div>
             </div>
             <div className="navbar-menu">
                 <a className="nav-item" href="/home">Home</a>
-                <a className="nav-item" href="/merch">Merch</a>
-            </div>
-            <div className="navbar-title">
-                Taylor Swift
+                <a className="nav-item" href="/about">Merch</a>
             </div>
         </nav>
     );
