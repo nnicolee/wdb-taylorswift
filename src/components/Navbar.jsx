ef4aa5e9b612a0183def424bb234f9fe8fd05139
Glitch (glitch-hello-react) <none>
2024-01-29 06:02:22 +0000

    🍓😋 Checkpoint ./src/styles/Navbar.css:36986232/2124 ./.glitch-assets:36986232/356 ./src/components/Navbar.jsx:36986232/1566

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,16 @@ const Navbar = () => {
 
     return (
         <nav className="navbar">
-            <ul>
-                <li className="nav-item" href="/home">Home</li>
-                <li className="nav-item" href="/about">Merch</li>
-            </ul>
+            <div className="navbar-logo">
+                <img src="https://cdn.glitch.global/4f194de8-562e-4c7b-bc7c-babcb839caf7/Vector.png?v=1706507904041" alt="Logo" />
+            </div>
+            <div className="navbar-menu">
+                <a className="nav-item" href="/home">Home</a>
+                <a className="nav-item" href="/merch">Merch</a>
+            </div>
+            <div className="navbar-title">
+                Taylor Swift
+            </div>
         </nav>
     );
 };
